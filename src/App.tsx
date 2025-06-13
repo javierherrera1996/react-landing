399af47bd74c2546829cd39955ce2b7eab0fde38
javierherrera <javier.herrera@davivienda.com>
2025-06-12 20:19:32 -0500

    Test Academia

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import './index.css';
 
 import React from 'react';
-import { LanguageProvider } from './context/LanguageContext';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
 const App: React.FC = () => {
   return (
-    <LanguageProvider>
-      <div className="min-h-screen bg-neutral-900">
-        <Header />
-        <Hero />
-      </div>
-    </LanguageProvider>
+    <div className="min-h-screen bg-neutral-900">
+      <Header />
+      <Hero />
+    </div>
   );
 };
 
